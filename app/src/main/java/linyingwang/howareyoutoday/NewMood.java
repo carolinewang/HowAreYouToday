5ae29f762239c6c1ac35fedffe33846d438d79b5
Caroline Wang <haylin2002@hotmail.com>
2015-07-28 21:19:17 -0400

    feat: Sad main layout done except the tag selection. Got word count part work. Got the new mood layout showing full screen without padding

--- app/src/main/java/linyingwang/howareyoutoday/NewMood.java
+++ app/src/main/java/linyingwang/howareyoutoday/NewMood.java
@@ -1,7 +1,9 @@
 package linyingwang.howareyoutoday;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewMood extends Activity {
 
@@ -9,6 +11,36 @@ public class NewMood extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_mood);
+	}
+
+	public void sad(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
+	}
+
+	public void happy(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
+	}
+
+	public void good(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
+	}
+
+	public void ok(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
+	}
+
+	public void crying(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
+	}
+
+	public void angry(View v) {
+		Intent intent = new Intent(this, Sad.class);
+		startActivity(intent);
 	}
 
 
